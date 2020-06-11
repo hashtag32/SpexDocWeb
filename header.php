c739a5ed3eb27fdffce6f3c8c3a2f491016d055e
Alexander Treib <alextreib94@gmail.com>
2020-06-11 10:16:17 +0200

    landing page

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 							twentytwenty_site_logo();
 
 							// Site description.
-							twentytwenty_site_description();
+							// twentytwenty_site_description();
 						?>
 
 					</div><!-- .header-titles -->
@@ -185,3 +185,5 @@
 		<?php
 		// Output the menu modal.
 		get_template_part( 'template-parts/modal-menu' );
+
+		
