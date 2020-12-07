c1cb038cb41ebd798c6856d6f195cec6f40718ad
Alexander Treib <alextreib94@gmail.com>
2020-12-07 19:50:08 +0100

    layout fixes

--- own-templates/template-front-page.php
+++ own-templates/template-front-page.php
@@ -193,30 +193,25 @@ get_header();
 			<div class="col-lg-6 h-100 text-center text-lg-left my-auto">
 				<ul class="list-inline mb-2">
 					<li class="list-inline-item">
-						<a href="#">About</a>
+						<a href="https://spexdoc.net/about-us">Über uns</a>
 					</li>
 					<li class="list-inline-item">&sdot;</li>
 					<li class="list-inline-item">
-						<a href="#">Contact</a>
+						<a href="https://spexdoc.net/contact">Kontakt</a>
 					</li>
 					<li class="list-inline-item">&sdot;</li>
 					<li class="list-inline-item">
-						<a href="#">Terms of Use</a>
+						<a href="https://spexdoc.net/datenschutzerklaerung">Datenschutzerklärung</a>
 					</li>
 					<li class="list-inline-item">&sdot;</li>
 					<li class="list-inline-item">
-						<a href="#">Privacy Policy</a>
+						<a href="https://spexdoc.net/legal-disclosure">Impressum</a>
 					</li>
 				</ul>
-				<p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
+				<p class="text-muted small mb-4 mb-lg-0">&copy; SpexDoc 2020. All Rights Reserved.</p>
 			</div>
 			<div class="col-lg-6 h-100 text-center text-lg-right my-auto">
 				<ul class="list-inline mb-0">
-					<li class="list-inline-item mr-3">
-						<a href="#">
-							<i class="fab fa-facebook fa-2x fa-fw"></i>
-						</a>
-					</li>
 					<li class="list-inline-item mr-3">
 						<a href="#">
 							<i class="fab fa-twitter-square fa-2x fa-fw"></i>
